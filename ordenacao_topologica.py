4624c460fb077e231c26031fae2c09080aaa9aa5
jresmer <jpresmer@gmail.com>
2023-10-23 17:24:38 -0300

    done

--- ordenacao_topologica.py
+++ ordenacao_topologica.py
@@ -3,22 +3,22 @@ from math import inf
 def dfs_visit(G, u, C, T, F, time, O):
     
     # u é marcado como visitado
-    C[u] = True
+    C[u-1] = True
     # tempo é inclementado
     time += 1
     # tempo de início da visita em u é marcado em T
-    T[u] = time
+    T[u-1] = time
 
     for v in G.neighbors(u):
 
-        if not C[v]:
+        if not C[v-1]:
 
             time = dfs_visit(G, v, C, T, F, time, O)
     
     time += 1
 
     # tempo do fim da visita em u é marcado em F
-    F[u] = time
+    F[u-1] = time
     # adiciona o vétice u ao início da lista
     O.insert(0, u)
 
@@ -41,8 +41,21 @@ def dfs(G):
 
     for u in G.vertices:
 
-        if not C[u]:
+        if not C[u-1]:
 
             time = dfs_visit(G, u, C, T, F, time, O)
     
     return O
+
+def ot_output(G):
+    
+    O = dfs(G)
+    ot_str = ""
+
+    for v in O:
+
+        ot_str += "{} → ".format(G.vertice(v).label)
+
+    ot_str = ot_str[:-3] + "."
+
+    print(ot_str)

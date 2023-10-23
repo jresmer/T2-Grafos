c267117002814c759d5bc9a32ff4f28d643946d7
jresmer <jpresmer@gmail.com>
2023-10-23 16:07:26 -0300

    done

--- kruskal.py
+++ kruskal.py
@@ -3,22 +3,36 @@ def kruskal(G):
     # árvore
     A = set()
     # subárvores
-    S = [set(v) for v in G.vertices]
+    S = [set()] * len(G.vertices)
+    for v in G.vertices: S[v.index - 1] = set((v.index, ))
     # arestas (ordenadas)
     E_ = sorted(G.edges, key=G.w)
 
     for u, v in E_:
 
-        if S[u] - S[v]:
+        if S[u-1] - S[v-1]:
 
             # adiciona a aresta a árvore
             A.add((u, v))
             # concatena Su e Sv
-            x = S[u] + S[v]
+            x = S[u-1].union(S[v-1])
 
             # atualiza as subárvores
             for y in x:
-                S[y] = x
+                S[y-1] = x
 
     return A
     
+def kruskal_output(G):
+
+    total_cost = 0
+    e_str = ""
+    
+    for u, v in kruskal(G):
+
+        total_cost += G.w((u,v))
+
+        e_str += "{}-{},".format(u, v)
+
+    print(total_cost)
+    print(e_str[:-1])

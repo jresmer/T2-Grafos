fb78d1932efe1f944c1f6c70d6c4848040d2b2a9
jresmer <jpresmer@gmail.com>
2023-10-22 23:11:51 -0300

    not tested

--- kruskal.py
+++ kruskal.py
@@ -1,2 +1,24 @@
 def kruskal(G):
-    pass
+
+    # árvore
+    A = set()
+    # subárvores
+    S = [set(v) for v in G.vertices]
+    # arestas (ordenadas)
+    E_ = sorted(G.edges, key=G.w)
+
+    for u, v in E_:
+
+        if S[u] - S[v]:
+
+            # adiciona a aresta a árvore
+            A.add((u, v))
+            # concatena Su e Sv
+            x = S[u] + S[v]
+
+            # atualiza as subárvores
+            for y in x:
+                S[y] = x
+
+    return A
+    

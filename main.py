18e659d9c7f50cd50c837f9a5d2e2cec8f174d82
jresmer <jpresmer@gmail.com>
2023-10-24 12:29:32 -0300

    added new way to run the program

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from os import path
 from componentes_fortemente_conexas import cfc_output
 from kruskal import kruskal_output
 from ordenacao_topologica import ot_output
@@ -20,30 +21,51 @@ def read(file_path: str) -> Graph or DiGraph:
     
 def main():
     if __name__ == "__main__":
+
+        # execução padrão
+        if len(sys.argv) == 1:
+            
+            # 1) Componentes fortemente conexas
+            directory = path.join("instances", "arvore_geradora_minima", "agm_tiny.net")
+            G = read(directory)
+            cfc_output(G)
+
+            # 2) Ordenação topologica
+            directory = path.join("instances", "extras", "ot_tiny.net")
+            G = read(directory)
+            ot_output(G)
+
+            # 3) Árvore geradora mínima (Kruskal)
+            directory = path.join("instances", "arvore_geradora_minima", "agm_tiny.net")
+            G = read(directory)
+            kruskal_output(G)
+
+        # execução: "Usage: python3 main.py [Directory] [algorithm]"
+        elif len(sys.argv) == 3:
         
-        if len(sys.argv) != 3:
-            sys.exit("Usage: python3 main.py [Directory] [algorithm]")
-        
-        directory = sys.argv[1]
-        algorith = sys.argv[2]
+            directory = sys.argv[1]
+            algorith = sys.argv[2]
 
-        G = read(directory)
+            G = read(directory)
 
-        match algorith:
+            match algorith:
 
-            # Componentes fortemente conexas
-            case "1":
-                cfc_output(G)
-            # Ordenação topologica
-            case "2":
-                ot_output(G)
-            # Árvore geradora mínima (Kruskal)
-            case "3":
-                kruskal_output(G)
-            # Todos
-            case _:
-                cfc_output(G)
-                ot_output(G)
-                kruskal_output(G)
+                # 1) Componentes fortemente conexas
+                case "1":
+                    cfc_output(G)
+                # 2) Ordenação topologica
+                case "2":
+                    ot_output(G)
+                # 3) Árvore geradora mínima (Kruskal)
+                case "3":
+                    kruskal_output(G)
+                # Todos
+                case _:
+                    cfc_output(G)
+                    ot_output(G)
+                    kruskal_output(G)
+        # erro
+        else:
+            sys.exit("Usage: python3 main.py or python3 main.py [Directory] [algorithm]")
 
 main()
